af2c4a5f640c45b4c19c122c2cf84043b53c08b5
Ajay S <ajay.s@hashedin.com>
2019-03-07 11:09:00 +0530

    Add navbar and avenir font

---
+++ src/components/AppNav/AppNav.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { createMuiTheme } from '@material-ui/core/styles';
+import createPalette from '@material-ui/core/styles/createPalette';
+import createTypography from '@material-ui/core/styles/createTypography';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+
+import './AppNav.css';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+const theme = createMuiTheme({
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      'Avenir',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+const navbarStyles = {
+  backgroundColor: '#fbfbfb',
+  color: '#000',
+  position: 'absolute',
+  fontFamily: "Comic Sans"
+};
+
+class AppNav extends Component {
+
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+
+    render() {
+      const { classes } = this.props;
+      const { auth, anchorEl } = this.state;
+      const open = Boolean(anchorEl);
+
+      return (
+        <MuiThemeProvider theme={theme}>
+        <div className={classes.root}>
+          {/* <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
+              }
+              label={auth ? 'Logout' : 'Login'}
+            />
+          </FormGroup> */}
+          <AppBar style={navbarStyles}>
+            <Toolbar>
+              {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton> */}
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                MAPTRACK
+              </Typography>
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-owns={open ? 'menu-appbar' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+        </MuiThemeProvider>
+      );
+    }
+}
+
+AppNav.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppNav);

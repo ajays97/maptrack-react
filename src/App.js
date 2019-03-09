e94d5a7d264e0aecc973f7243383f550c34cf525
Lucifer <ramanandsirvi21@gmail.com>
2019-03-09 23:58:22 +0530

    SideNav

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React, { Component } from "react";
 import MainMap from "./components/MainMap/MainMap";
 import AppNav from "./components/AppNav/AppNav";
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
-import './app.css';
-import { history } from './helpers/history';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import "./app.css";
+import { history } from "./helpers/history";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import LoginPage from "./components/LoginPage/LoginPage";
-import HomePage from './components/HomePage/HomePage';
+import HomePage from "./components/HomePage/HomePage";
+import SideNav from "./components/SideNav/SideNav";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     const { dispatch } = this.props;
 
-    history.listen((location, action) => {
-      
-    });
-
+    history.listen((location, action) => {});
   }
-  
+
   render() {
     return (
       <div>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Switch>
             <PrivateRoute path="/" exact component={HomePage} />
             <Route path="/login" component={LoginPage} />
           </Switch>
 
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <SideNav />
       </div>
     );
   }
